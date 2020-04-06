aa88cacc653333bff6e0ef7415860c71ef63f05f
Tarun Saxena <48789723+SaxenaTarun@users.noreply.github.com>
2020-04-06 20:38:57 +0530

    by tarun

--- Project/app/src/main/java/com/example/project/Login.java
+++ Project/app/src/main/java/com/example/project/Login.java
@@ -1,6 +1,8 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,4 +10,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.login);
-}}
+}
+
+    public void nextpage(View view) {
+        Intent home= new Intent(getApplicationContext(),Userhome.class);
+        startActivity( home );
+    }
+}
