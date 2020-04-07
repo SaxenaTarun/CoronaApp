bf21e037c8d61ad5c31a34b1c26c517299ce72d0
imnik11 <nitesh4y2001@gmail.com>
2020-04-07 15:12:19 +0530

    nav bars for user

--- Project/app/src/main/java/com/example/project/Login.java
+++ Project/app/src/main/java/com/example/project/Login.java
@@ -1,11 +1,24 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.login);
-}}
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, user_nav.class));
+            }
+        });
+}
+}
