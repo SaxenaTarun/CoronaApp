759557276b280f31ef327cd33f33657831ae7b91
imnik11 <nitesh4y2001@gmail.com>
2020-04-11 20:05:08 +0530

    data fetch from firestore added!

--- Project/app/src/main/java/com/example/project/AuthorityHome.java
+++ Project/app/src/main/java/com/example/project/AuthorityHome.java
@@ -2,6 +2,7 @@ package com.example.project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -9,41 +10,81 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class AuthorityHome extends AppCompatActivity {
+
+    private static final String TAG ="Authority" ;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.authority_home);
 
-        BottomNavigationView bottomNavigationView1=findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView1.setSelectedItemId(R.id.homeVol);
-
-        bottomNavigationView1.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                switch (item.getItemId())
-                {
-                    case R.id.profileVol:
-                        startActivity(new Intent(getApplicationContext(),VolunteerProfile.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.statsVol:
-                        startActivity(new Intent(getApplicationContext(),VolunteerStats.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.homeVol:
-                        return true;
-
-                }
-
-
-
-                return false;
-            }
-        });
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+//        DocumentReference docRef = db.collection("users").document();
+//        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                if (task.isSuccessful()) {
+//                    DocumentSnapshot document = task.getResult();
+//                    if (document.exists()) {
+//                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+//                    } else {
+//                        Log.d(TAG, "No such document");
+//                    }
+//                } else {
+//                    Log.d(TAG, "get failed with ", task.getException());
+//                }
+//            }
+//        });
+//        BottomNavigationView bottomNavigationView1=findViewById(R.id.bottom_navigation);
+//
+//        bottomNavigationView1.setSelectedItemId(R.id.homeVol);
+//
+//        bottomNavigationView1.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//                switch (item.getItemId())
+//                {
+//                    case R.id.profileVol:
+//                        startActivity(new Intent(getApplicationContext(),VolunteerProfile.class));
+//                        overridePendingTransition(0,0);
+//                        return true;
+//                    case R.id.statsVol:
+//                        startActivity(new Intent(getApplicationContext(),VolunteerStats.class));
+//                        overridePendingTransition(0,0);
+//                        return true;
+//                    case R.id.homeVol:
+//                        return true;
+//
+//                }
+//
+//
+//
+//                return false;
+//            }
+//        });
 
 
     }
