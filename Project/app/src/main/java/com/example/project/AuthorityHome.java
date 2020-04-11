9a1e31b05bbcdde1fed698272793cdfc46c14b76
Tarun Saxena <48789723+SaxenaTarun@users.noreply.github.com>
2020-04-11 19:54:51 +0530

    7:54pm

--- Project/app/src/main/java/com/example/project/AuthorityHome.java
+++ Project/app/src/main/java/com/example/project/AuthorityHome.java
@@ -16,50 +16,14 @@ public class AuthorityHome extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.authority_home);
 
-        BottomNavigationView bottomNavigationView1=findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView1.setSelectedItemId(R.id.homeVol);
-
-        bottomNavigationView1.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                switch (item.getItemId())
-                {
-                    case R.id.profileVol:
-                        startActivity(new Intent(getApplicationContext(),VolunteerProfile.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.statsVol:
-                        startActivity(new Intent(getApplicationContext(),VolunteerStats.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.homeVol:
-                        return true;
-
-                }
 
 
 
-                return false;
-            }
-        });
 
 
     }
-    public void onYes(View view)
-    {
-        Intent yes=new Intent(getApplicationContext(),Organizations.class);
-        startActivity(yes);
-
-    }
-
-    public void onNo(View view)
-    {
-        Intent no=new Intent(getApplicationContext(),VolunteerProfile.class);
-        startActivity(no);
-
-        Toast.makeText(getApplicationContext(),"Thank you for considering to volunteer.\nLogged Out Successfully.",Toast.LENGTH_SHORT).show();
-
+    public void vd(View view){
+        startActivity(new Intent(AuthorityHome.this, Data.class));
+        finish();
     }
 }
